f9514704e4e878fe418b7d9eeeebaca50753ec16
RS Finance Labs <52669170+RS-Finance@users.noreply.github.com>
2021-09-16 10:33:35 -0700

    2nd

---
+++ precache-manifest.43af0b87c5b33c8e06b4e6456e5371f3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58a59a2818600f45da995a733a0a3029",
+    "url": "/rs-info/index.html"
+  },
+  {
+    "revision": "ab9891ea40a3fdd54eba",
+    "url": "/rs-info/static/js/2.e70d5a4c.chunk.js"
+  },
+  {
+    "revision": "90c0b6da4a9af2bf8e5ab0961c02b768",
+    "url": "/rs-info/static/js/2.e70d5a4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f07c46367bcba724e4ff",
+    "url": "/rs-info/static/js/main.99ce8883.chunk.js"
+  },
+  {
+    "revision": "1b3e7989217f6189df97",
+    "url": "/rs-info/static/js/runtime-main.772a1f3e.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/rs-info/static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "ae30668a2905d5ceb77bf0821fe6af64",
+    "url": "/rs-info/static/media/logo.ae30668a.svg"
+  },
+  {
+    "revision": "ae30668a2905d5ceb77bf0821fe6af64",
+    "url": "/rs-info/static/media/logo_white.ae30668a.svg"
+  },
+  {
+    "revision": "a8987e476fd50f35a4ac32598f511358",
+    "url": "/rs-info/static/media/wordmark_white.a8987e47.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.fd5326bbf06e0845e209cd24fcc43491.js"
+  "/rs-info/precache-manifest.43af0b87c5b33c8e06b4e6456e5371f3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rs-info/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
