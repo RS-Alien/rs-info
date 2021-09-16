358a573dc7e72057730e340b18a6364eb5d204c7
RS Finance Labs <52669170+RS-Finance@users.noreply.github.com>
2021-09-16 09:55:03 -0700

    1st

---
+++ precache-manifest.fd5326bbf06e0845e209cd24fcc43491.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7534071a6d86774881e4379536908d5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8ba0faa5d8e65d3618b8",
+    "url": "./static/js/2.4a16c13a.chunk.js"
+  },
+  {
+    "revision": "90c0b6da4a9af2bf8e5ab0961c02b768",
+    "url": "./static/js/2.4a16c13a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f07c46367bcba724e4ff",
+    "url": "./static/js/main.99ce8883.chunk.js"
+  },
+  {
+    "revision": "23299b0513433ec88135",
+    "url": "./static/js/runtime-main.80553221.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "./static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "ae30668a2905d5ceb77bf0821fe6af64",
+    "url": "./static/media/logo.ae30668a.svg"
+  },
+  {
+    "revision": "ae30668a2905d5ceb77bf0821fe6af64",
+    "url": "./static/media/logo_white.ae30668a.svg"
+  },
+  {
+    "revision": "a8987e476fd50f35a4ac32598f511358",
+    "url": "./static/media/wordmark_white.a8987e47.svg"
+  }
+]);
